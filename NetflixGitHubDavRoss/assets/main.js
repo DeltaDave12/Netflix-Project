2a72d36db8a4e39a5d5eef60d538dea83ae94fbd
DeltaDave12 <91453740+DeltaDave12@users.noreply.github.com>
2022-03-16 16:29:56 +0100

    Ajout de certaines images + javascript

---
+++ NetflixGitHubDavRoss/assets/main.js
@@ -0,0 +1,95 @@
+//Je sais que cela fait beacoup de DRY code mais par manque de temps
+// et parce que j'ai pris trop de temps à trouver une fonction qui 
+// marche je n'ai pas tt de suite envie de rendre le code plus lisible 
+
+//Carousel 1
+function michel(){
+    let div1 = document.querySelector('.scroll-left')
+    div1.scrollLeft += 250
+}
+
+function jacques(){
+    let div1 = document.querySelector('.scroll-right')
+    div1.scrollLeft -= 250
+}
+
+
+//Carousel 2
+function michel2(){
+    let div1 = document.querySelector('.scroll-left2')
+    div1.scrollLeft += 250
+}
+
+function jacques2(){
+    let div1 = document.querySelector('.scroll-right2')
+    div1.scrollLeft -= 250
+}
+
+
+//Carousel 3
+function michel3(){
+    let div1 = document.querySelector('.scroll-left3')
+    div1.scrollLeft += 250
+}
+
+function jacques3(){
+    let div1 = document.querySelector('.scroll-right3')
+    div1.scrollLeft -= 250
+}
+
+
+//Carousel 4
+function michel4(){
+    let div1 = document.querySelector('.scroll-left4')
+    div1.scrollLeft += 250
+}
+
+function jacques4(){
+    let div1 = document.querySelector('.scroll-right4')
+    div1.scrollLeft -= 250
+}
+
+//Carousel 5
+function michel5(){
+    let div1 = document.querySelector('.scroll-left5')
+    div1.scrollLeft += 250
+}
+
+function jacques5(){
+    let div1 = document.querySelector('.scroll-right5')
+    div1.scrollLeft -= 250
+}
+
+//Carousel 6
+function michel6(){
+    let div1 = document.querySelector('.scroll-left6')
+    div1.scrollLeft += 250
+}
+
+function jacques6(){
+    let div1 = document.querySelector('.scroll-right6')
+    div1.scrollLeft -= 250
+}
+
+//Carousel 7
+function michel7(){
+    let div1 = document.querySelector('.scroll-left7')
+    div1.scrollLeft += 250
+}
+
+function jacques7(){
+    let div1 = document.querySelector('.scroll-right7')
+    div1.scrollLeft -= 250
+}
+
+//Carousel 8
+function michel8(){
+    let div1 = document.querySelector('.scroll-left8')
+    div1.scrollLeft += 250
+}
+
+function jacques8(){
+    let div1 = document.querySelector('.scroll-right8')
+    div1.scrollLeft -= 250
+}
+
