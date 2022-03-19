a14f2b6aa3c4876473b235cfc9b412a43bdfdab3
DeltaDave12 <91453740+DeltaDave12@users.noreply.github.com>
2022-03-19 23:49:31 +0100

    NEW JS AND ADDED IMAGES

--- NetflixGitHubDavRoss/assets/main.js
+++ NetflixGitHubDavRoss/assets/main.js
@@ -2,6 +2,7 @@
 // et parce que j'ai pris trop de temps à trouver une fonction qui 
 // marche je n'ai pas tt de suite envie de rendre le code plus lisible 
 
+/*Carousel Functions*/
 //Carousel 1
 function michel(){
     let div1 = document.querySelector('.scroll-left')
@@ -91,5 +92,109 @@ function michel8(){
 function jacques8(){
     let div1 = document.querySelector('.scroll-right8')
     div1.scrollLeft -= 250
+}
+
+/*End Carousel Functions*/
+
+/* Random image function */
+
+window.onload = RandomImage1
+
+var type = document.getElementsByTagName("img")
+
+/*Section 1*/ 
+let image_array1 = ["./assets/r6/aruni.jpg", 
+"./assets/r6/frost.jpg", 
+"./assets/r6/fuze.jpeg",
+"./assets/r6/castle.jpg",
+"./assets/r6/nokk.jpg",
+"./assets/r6/pulse.jpg",
+"./assets/r6/iq.jpg",
+"./assets/r6/jackal.jpg",
+"./assets/r6/kapkan.jpg", 
+"./assets/r6/maestro.jpg",
+"./assets/r6/maverick.jpg",
+"./assets/r6/melusi.jpg",
+"./assets/r6/ace.jpg",
+"./assets/r6/mozzie.jpg",
+"./assets/r6/mira.jpg",
+"./assets/r6/osa.png",
+"./assets/r6/grid.jpg",
+"./assets/r6/ash.jpg",
+"./assets/r6/sledge.jpg",
+"./assets/r6/warden.jpg"] 
+
+
+function RandomImage1(){
+    let index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random11').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random12').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random13').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random14').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random15').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random16').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random17').src = image_array1[index];
+
+    index = Math.floor(Math.random()*image_array1.length)
+    document.querySelector('.random18').src = image_array1[index];
+}
+
+
+/*Section 2*/ 
+let image_array2 = ["./assets/r6/bank.jpg", 
+"./assets/r6/border.jpg", 
+"./assets/r6/chalet.jpg",
+"./assets/r6/clubhouse.jpg",
+"./assets/r6/coastline.jpg",
+"./assets/r6/frontiere.jpg",
+"./assets/r6/oregon.jpg",
+"./assets/r6/theme.jpg",
+"./assets/r6/villa.jpg", 
+"./assets/r6/consulate.jpg",
+"./assets/r6/cafe.jpg",
+"./assets/r6/maison.jpg",
+"./assets/r6/outback.jpg",
+"./assets/r6/avion.jpg",
+"./assets/r6/gratte.jpg",
+"./assets/r6/favela.jpg",
+"./assets/r6/yacht.jpg",] 
+
+
+function RandomImage2(){
+    let index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random21').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random22').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random23').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random24').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random25').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random26').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random27').src = image_array2[index];
+
+    index = Math.floor(Math.random()*image_array2.length)
+    document.querySelector('.random28').src = image_array2[index];
 }
 
